af4368107295362e4ec1dfdda113f310779fa995
Sobia Ahmad <sobia_ahmad_94@outlook.com>
2020-09-04 21:57:39 +0100

    Updates

---
+++ precache-manifest.a4ded66150eb9c5a6ad94f02b09f0ba8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9051bc2f527f5fe75cc8c621be758150",
+    "url": "/react-js-database/index.html"
+  },
+  {
+    "revision": "b6f4a7983329289a7c5c",
+    "url": "/react-js-database/static/css/main.b5cd8efa.chunk.css"
+  },
+  {
+    "revision": "0d5343da57e238b054b3",
+    "url": "/react-js-database/static/js/2.4942de4c.chunk.js"
+  },
+  {
+    "revision": "6b1c9aa1f24077e4bfd4b51db2988c14",
+    "url": "/react-js-database/static/js/2.4942de4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6f4a7983329289a7c5c",
+    "url": "/react-js-database/static/js/main.e6cef414.chunk.js"
+  },
+  {
+    "revision": "9381283cfa023b7a6d10",
+    "url": "/react-js-database/static/js/runtime-main.6b534222.js"
+  }
+]);
